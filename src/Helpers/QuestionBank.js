5ed18b4fd51eab64d31095ee00f6017da87ad1c0
anjalirughani01 <anjalirughani01@gmail.com>
2023-05-11 18:13:57 +0530

    quiz app

---
+++ src/Helpers/QuestionBank.js
@@ -0,0 +1,34 @@
+export const Questions = [
+    {
+        prompt: "what is 1+3",
+        optionA:"1",
+        optionB: "22",
+        optionC: "4",
+        optionD: "10",
+        answer: "C"
+    },
+    {
+        prompt: "what is 1+10",
+        optionA:"1",
+        optionB: "22",
+        optionC: "11",
+        optionD: "10",
+        answer: "C"
+    },
+    {
+        prompt: "what is 21+32",
+        optionA:"1",
+        optionB: "22",
+        optionC: "53",
+        optionD: "10",
+        answer: "C"
+    },
+    {
+        prompt: "what is 101+3",
+        optionA:"1",
+        optionB: "22",
+        optionC: "104",
+        optionD: "10",
+        answer: "C"
+    }
+]
